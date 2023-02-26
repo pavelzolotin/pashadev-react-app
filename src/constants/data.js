ecb258f7c3d60aec54f189ae2c5432379543b17c
Pavel Zolotin <zolotinpavel@gmail.com>
2023-02-26 14:39:35 +0500

    refactoring About cards component

--- src/constants/data.js
+++ src/constants/data.js
@@ -45,3 +45,51 @@ export const projects = [
         link_second: 'https://mad-metaverse.netlify.app/'
     }
 ];
+
+export const cards = [
+    {
+        id: 1,
+        image: require('../img/html-logo.png'),
+        title: 'HTML'
+    },
+    {
+        id: 2,
+        image: require('../img/scss-logo.png'),
+        title: 'SCSS'
+    },
+    {
+        id: 3,
+        image: require('../img/js-logo.png'),
+        title: 'JavaScript'
+    },
+    {
+        id: 4,
+        image: require('../img/typescript-logo.png'),
+        title: 'TypeScript'
+    },
+    {
+        id: 5,
+        image: require('../img/react-js-logo.png'),
+        title: 'React JS'
+    },
+    {
+        id: 6,
+        image: require('../img/redux-logo.png'),
+        title: 'Redux'
+    },
+    {
+        id: 7,
+        image: require('../img/node-js-logo.png'),
+        title: 'Node JS'
+    },
+    {
+        id: 8,
+        image: require('../img/git-logo.png'),
+        title: 'Git'
+    },
+    {
+        id: 9,
+        image: require('../img/figma-logo.png'),
+        title: 'Figma'
+    }
+];
