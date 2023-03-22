42c6bda73ae96af63a7a352b8f8cd6fdb2f4f96a
Pavel Zolotin <zolotinpavel@gmail.com>
2023-03-22 11:24:19 +0500

    fix eslint error in Cursor component

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,4 +1,4 @@
-import {useRef, useEffect} from 'react';
+import {useRef, useEffect, useCallback} from 'react';
 import styled from 'styled-components';
 
 const CursorDot = styled.div`
@@ -14,7 +14,7 @@ const CursorDot = styled.div`
   opacity: 1;
   transition: width .3s, height .3s, transform .3s;
   z-index: 10;
-  
+
   @media (max-width: 767px) {
     display: none;
   }
@@ -40,28 +40,6 @@ const Cursor = () => {
     const requestRef = useRef(null);
     const delay = 12;
 
-    useEffect(() => {
-        document.addEventListener('mousedown', mouseOverEvent);
-        document.addEventListener('mouseup', mouseOutEvent);
-        document.addEventListener('mousemove', mouseMoveEvent);
-        document.addEventListener('mouseenter', mouseEnterEvent);
-        document.addEventListener('mouseleave', mouseLeaveEvent);
-
-        animateDotOutline();
-        handleLinkHovers();
-
-        return () => {
-            document.removeEventListener('mousedown', mouseOverEvent);
-            document.removeEventListener('mouseup', mouseOutEvent);
-            document.removeEventListener('mousemove', mouseMoveEvent);
-            document.removeEventListener('mouseenter', mouseEnterEvent);
-            document.removeEventListener('mouseleave', mouseLeaveEvent);
-
-            cancelAnimationFrame(requestRef.current);
-        };
-        // eslint-disable-next-line
-    }, []);
-
     const toggleCursorVisibility = () => {
         if (cursorVisible.current) {
             dot.current.style.opacity = 1;
@@ -80,64 +58,90 @@ const Cursor = () => {
         }
     };
 
-    const handleLinkHovers = () => {
-        document.querySelectorAll('a, button').forEach(el => {
-            el.addEventListener('mouseover', () => {
-                cursorEnlarged.current = true;
-                dotOutline.current.classList.add('dot-color');
-                dotOutline.current.style.transition = 'width .3s, height .3s, background-color .5s';
-                toggleCursorSize();
+    const handleLinkHovers = useCallback(() => {
+            document.querySelectorAll('a, button').forEach(el => {
+                el.addEventListener('mouseover', () => {
+                    cursorEnlarged.current = true;
+                    dotOutline.current.classList.add('dot-color');
+                    dotOutline.current.style.transition = 'width .3s, height .3s, background-color .5s';
+                    toggleCursorSize();
+                });
+
+                el.addEventListener('mouseleave', () => {
+                    cursorEnlarged.current = false;
+                    dotOutline.current.classList.remove('dot-color');
+                    dotOutline.current.style.transition = 'width .3s, height .3s, background-color .5s';
+                    toggleCursorSize();
+                });
             });
+        }, []
+    );
 
-            el.addEventListener('mouseleave', () => {
-                cursorEnlarged.current = false;
-                dotOutline.current.classList.remove('dot-color');
-                dotOutline.current.style.transition = 'width .3s, height .3s, background-color .5s';
-                toggleCursorSize();
-            });
-        });
-    };
+    const mouseOverEvent = useCallback(() => {
+            cursorEnlarged.current = true;
+            toggleCursorSize();
+        }, []
+    );
 
-    const mouseOverEvent = () => {
-        cursorEnlarged.current = true;
-        toggleCursorSize();
-    };
+    const mouseOutEvent = useCallback(() => {
+            cursorEnlarged.current = false;
+            toggleCursorSize();
+        }, []
+    );
 
-    const mouseOutEvent = () => {
-        cursorEnlarged.current = false;
-        toggleCursorSize();
-    };
+    const mouseEnterEvent = useCallback(() => {
+            cursorVisible.current = true;
+            toggleCursorVisibility();
+        }, []
+    );
 
-    const mouseEnterEvent = () => {
-        cursorVisible.current = true;
-        toggleCursorVisibility();
-    };
+    const mouseLeaveEvent = useCallback(() => {
+            cursorVisible.current = false;
+            toggleCursorVisibility();
+        }, []
+    );
 
-    const mouseLeaveEvent = () => {
-        cursorVisible.current = false;
-        toggleCursorVisibility();
-    };
+    const mouseMoveEvent = useCallback((e) => {
+            cursorVisible.current = true;
+            toggleCursorVisibility();
 
-    const mouseMoveEvent = (e) => {
-        cursorVisible.current = true;
-        toggleCursorVisibility();
+            endX.current = e.clientX;
+            endY.current = e.clientY;
 
-        endX.current = e.clientX;
-        endY.current = e.clientY;
+            dot.current.style.top = endY.current + 'px';
+            dot.current.style.left = endX.current + 'px';
+        }, []
+    );
 
-        dot.current.style.top = endY.current + 'px';
-        dot.current.style.left = endX.current + 'px';
-    };
+    const animateDotOutline = useCallback(() => {
+            _x.current += (endX.current - _x.current) / delay;
+            _y.current += (endY.current - _y.current) / delay;
 
-    const animateDotOutline = () => {
-        _x.current += (endX.current - _x.current) / delay;
-        _y.current += (endY.current - _y.current) / delay;
+            dotOutline.current.style.top = _y.current + 'px';
+            dotOutline.current.style.left = _x.current + 'px';
 
-        dotOutline.current.style.top = _y.current + 'px';
-        dotOutline.current.style.left = _x.current + 'px';
+            requestRef.current = requestAnimationFrame(animateDotOutline);
+        }, []
+    );
 
-        requestRef.current = requestAnimationFrame(animateDotOutline);
-    };
+    useEffect(() => {
+        document.addEventListener('mousedown', mouseOverEvent);
+        document.addEventListener('mouseup', mouseOutEvent);
+        document.addEventListener('mousemove', mouseMoveEvent);
+        document.addEventListener('mouseenter', mouseEnterEvent);
+        document.addEventListener('mouseleave', mouseLeaveEvent);
+        animateDotOutline();
+        handleLinkHovers();
+
+        return () => {
+            document.removeEventListener('mousedown', mouseOverEvent);
+            document.removeEventListener('mouseup', mouseOutEvent);
+            document.removeEventListener('mousemove', mouseMoveEvent);
+            document.removeEventListener('mouseenter', mouseEnterEvent);
+            document.removeEventListener('mouseleave', mouseLeaveEvent);
+            cancelAnimationFrame(requestRef.current);
+        };
+    }, [animateDotOutline, handleLinkHovers, mouseOverEvent, mouseOutEvent, mouseMoveEvent, mouseEnterEvent, mouseLeaveEvent]);
 
     return (
         <>
